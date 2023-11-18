4fc648f61ed7ecbbf00b899f10cc9c7ad5862236
Angeline Tran <angelinenhanaitran@gmail.com>
2023-11-18 15:44:01 -0700

    Added profile pages

---
+++ app/src/Pages/RenterProfile.jsx
@@ -0,0 +1,144 @@
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../Images/logo.png";
+function RenterProfile(){
+    return(
+      <div className="div">
+        <div className="div-2">
+          <div className="div-3">
+            <div className="div-4">
+              <div className="div-5">
+              <Link to="/">
+              <img
+                class="group-6-T94"
+                src={logo}
+                alt={logo}
+                id="I141:2772;1:110"
+              />
+            </Link>
+                <div className="div-6">
+                  <div className="div-7">walrus</div>
+                  <div className="div-8">RENTING GROUP</div>
+                </div>
+              </div>
+              <div className="div-9">
+                <div className="div-10">
+                  <div className="div-11">
+                    <div className="div-12">Where</div>
+                    <div className="div-13">Find your new home</div>
+                  </div>
+                  <div className="div-14">
+                    <div className="div-15" />
+                    <div className="div-16">
+                      <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/ecc478b9-d581-45d6-82b0-669e236f84f0?apiKey=afe4f9d270b143e39e8b35be5ef69cea&"
+                        className="img-2"
+                      />
+                      <div className="div-17">Beds</div>
+                    </div>
+                    <div className="div-18" />
+                    <div className="div-19">
+                      <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/aae25e49-5b93-4ffc-8d65-4ab5ff1fd73b?apiKey=afe4f9d270b143e39e8b35be5ef69cea&"
+                        className="img-3"
+                      />
+                      <div className="div-20">Bath</div>
+                    </div>
+                    <div className="div-21" />
+                    <div className="div-22">
+                      <div className="div-23">Price Range</div>
+                      <div className="div-24">Min - Max</div>
+                    </div>
+                  </div>
+                </div>
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/5122d754-3028-41ca-9b11-5266323047ef?apiKey=afe4f9d270b143e39e8b35be5ef69cea&"
+                  className="img-4"
+                />
+              </div>
+              <div className="div-25">
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/072611d8-64cb-4bfc-b360-b0b197c20700?apiKey=afe4f9d270b143e39e8b35be5ef69cea&"
+                  className="img-5"
+                />
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/c3b264a9-23e7-410e-98fa-fd4e1d7de5b3?apiKey=afe4f9d270b143e39e8b35be5ef69cea&"
+                  className="img-6"
+                />
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/622a4d24-617a-40a8-80cd-99972c226ab6?apiKey=afe4f9d270b143e39e8b35be5ef69cea&"
+                  className="img-7"
+                />
+                <div className="div-26">Logout</div>
+              </div>
+            </div>
+            <div className="div-27">Additional Filters</div>
+          </div>
+        </div>
+        <div className="div-28">
+          <div className="div-29">
+            <div className="div-30">
+              <div className="column">
+                <img
+                  loading="lazy"
+                  srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/fda63b65-67f0-4a34-945c-9df7d3da79fa?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/fda63b65-67f0-4a34-945c-9df7d3da79fa?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/fda63b65-67f0-4a34-945c-9df7d3da79fa?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/fda63b65-67f0-4a34-945c-9df7d3da79fa?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/fda63b65-67f0-4a34-945c-9df7d3da79fa?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/fda63b65-67f0-4a34-945c-9df7d3da79fa?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/fda63b65-67f0-4a34-945c-9df7d3da79fa?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/fda63b65-67f0-4a34-945c-9df7d3da79fa?apiKey=afe4f9d270b143e39e8b35be5ef69cea&"
+                  className="img-8"
+                />
+              </div>
+              <div className="column-2">
+                <div className="div-31">
+                  <div className="div-32">Lulu Lemon</div>
+                  <div className="div-33">22 years old</div>
+                  <div className="div-34">
+                    I am a short term renter looking for a home with a near by
+                    Pilates studio.
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    #renter
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="div-35">
+          <div className="div-36">
+            <div className="column-3">
+              <div className="div-37">
+                <div className="div-38">Employment</div>
+                <div className="div-39">
+                  <div className="div-40">Full Time Student</div>
+                  <div className="div-41">University of Calgary</div>
+                </div>
+                <div className="div-42">Engineering</div>
+              </div>
+            </div>
+            <div className="column-4">
+              <div className="div-43">
+                <div className="div-44">Looking for</div>
+                <div className="div-45">
+                  <div className="div-46">Room to Rent</div>
+                  <div className="div-47">Short Term</div>
+                  <div className="div-48">Near University</div>
+                </div>
+                <div className="div-49">
+                  <div className="div-50">Access to Transit</div>
+                  <div className="div-51">Ensuite Laundry</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+export default RenterProfile;
