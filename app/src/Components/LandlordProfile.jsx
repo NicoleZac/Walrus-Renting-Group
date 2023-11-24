628cecf1b1cc582e9e7bbc8fdc17c4e3630126f1
Angeline Tran <angelinenhanaitran@gmail.com>
2023-11-23 22:48:06 -0700

    Fixed renter profile page

--- app/src/Components/LandlordProfile.jsx
+++ app/src/Components/LandlordProfile.jsx
@@ -7,26 +7,22 @@ const LandlordProfile = ({ user, openPopup }) => {
   const { firstName, lastName, email } = user;
 
   return (
-    <div className="main-container">
-      <div className="image-wrapper">
-        <div className="column">
+    <div className="profile-card">
+      <div className="profile-image">
           <img
             loading="lazy"
             srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/78272d46-c19e-4e04-a919-07976ce0bb4f?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/78272d46-c19e-4e04-a919-07976ce0bb4f?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/78272d46-c19e-4e04-a919-07976ce0bb4f?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/78272d46-c19e-4e04-a919-07976ce0bb4f?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/78272d46-c19e-4e04-a919-07976ce0bb4f?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/78272d46-c19e-4e04-a919-07976ce0bb4f?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/78272d46-c19e-4e04-a919-07976ce0bb4f?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/78272d46-c19e-4e04-a919-07976ce0bb4f?apiKey=afe4f9d270b143e39e8b35be5ef69cea&"
-            className="property-image"
+            className="profile-img"
             alt="Profile Image"
           />
-        </div>
-        <div className="column">
-          <div className="profile-information">
-            <h2 className="profile-name">{`${firstName} ${lastName}`}</h2>
-            <p className="profile-email">{`${email}`}</p>
-            <p className="profile-age">65 years old</p>
-            <p className="profile-description">
-              I’ve got a nice property for you to stay at. Looking for renters.
-            </p>
-          </div>
-        </div>
+      </div>
+      <div className="profile-details">
+        <h2 className="profile-name">{`${firstName} ${lastName}`}</h2>
+        <p className="profile-age">65 years old</p>
+        <p className="profile-email">{`${email}`}</p>
+        <p className="profile-description">
+          I’ve got a nice property for you to stay at. Looking for renters.
+        </p>
       </div>
 
       {/* Call PropertyComponent to display the property listing */}
@@ -39,15 +35,15 @@ function PropertyComponent(openPopup) {
   return (
     <header className="main-container">
       <h2 className="title">My properties</h2>
-      <div className="create-property">
-        <div
+      <span className="create-property">
+        <div 
           onClick={openPopup}
-          className="submit-UJ6"
+          className="button"
           id="I195:7530;195:6735"
         >
           Create Property
         </div>
-      </div>
+      </span>
       <div className="current-properties">
         <div className="landlords-own-properties-LbC" id="I195:7530;195:7168">
           <div className="employment-Ega" id="I195:7530;195:7168;161:3104">
--- app/src/Components/RenterProfile.jsx
+++ app/src/Components/RenterProfile.jsx
@@ -1,15 +1,18 @@
-import React, { useContext} from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
-import "./RenterProfile.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./RenterProfile.css";
 
 const RenterProfile = ({ user }) => {
-    const { firstName, lastName, email} = user;
-    return (
+  const { firstName, lastName, email } = user;
+
+  return (
+    <div>
       <div className="profile-card">
         <div className="profile-image">
           <img
             loading="lazy"
-            srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&"className="profile-img"
+            srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/9cb6afc9-b144-491f-96d2-9fbbdfea796a?apiKey=afe4f9d270b143e39e8b35be5ef69cea&"
+            className="profile-img"
             alt="Profile Image"
           />
         </div>
@@ -17,36 +20,47 @@ const RenterProfile = ({ user }) => {
           <h2 className="profile-name">{`${firstName} ${lastName}`}</h2>
           <div className="profile-age">22 years old</div>
           <p className="profile-email">{`${email}`}</p>
-          <div className="profile-description">I am a short term renter looking for a home with a nearby Pilates studio.</div>
-          <div className="send-message">Send Message</div>
-      <div className="additional-information">
-        <div className="column-3">
-          <div className="div-37">
-            <div className="employment">Employment</div>
-            <div className="div-39">
-              <div className="student">Full Time Student</div>
-              <div className="university">University of Calgary</div>
-              <div className="faculty">Engineering</div>
-            </div>
+          <div className="profile-description">
+            I am a short-term renter looking for a home with a nearby Pilates
+            studio.
           </div>
-        </div>
-        <div className="filters">
-          <div className="div-43">
-            <div className="requirements">Looking for</div>
-            <div className="div-45">
-              <div className="room">Room to Rent</div>
-              <div className="length">Short Term</div>
-              <div className="location">Near University</div>
-            </div>
-            <div className="div-49">
-              <div className="transit">Access to Transit</div>
-              <div className="laundry">Ensuite Laundry</div>
-            </div>
+          <div className="send-message">Send Message
+            <span className="link"> 
+              <Link to={`/UserProfile/${email}`}>
+                    <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/248ce8f4-51d8-4320-a68c-8957a496b0bd?"
+                      className="message"
+                    />
+              </Link>
+            </span>
           </div>
         </div>
       </div>
+      <div className="occupation">
+        <div className="div-37">
+          <div className="employment">Employment</div>
+          <div className="div-39">
+            <div className="student">Full-Time Student</div>
+            <div className="university">University of Calgary</div>
+            <div className="faculty">Engineering</div>
+          </div>
+        </div>
+      </div>
+      <div className="filters">
+        <div className="div-43">
+          <div className="requirements">Looking for</div>
+          <div className="div-45">
+            <div className="room">Room to Rent</div>
+            <div className="length">Short Term</div>
+            <div className="location">Near University</div>
+            <div className="transit">Access to Transit</div>
+            <div className="laundry">Ensuite Laundry</div>
+          </div>
+        </div>
       </div>
     </div>
-    );
+  );
 };
+
 export default RenterProfile;
