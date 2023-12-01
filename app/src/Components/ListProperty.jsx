91f3e19e8ed1dede0f4ecc4812ea8c4270c6fe6c
ericw30331 <ericw30331@gmail.com>
2023-12-01 15:12:01 -0700

    fix double scroll bar issue

--- app/src/Components/ListProperty.jsx
+++ app/src/Components/ListProperty.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import Modal from 'react-modal';
 import Page1 from './ListPropertyPages/Page1';
 import Page2 from './ListPropertyPages/Page2';
@@ -84,17 +84,21 @@ const ListProperty =({isOpen,requestClose})=>{
                 return <Page1 onNext={handleNext}  requestClose={requestClose}/>;
         }
     }
+
     return(
 <Modal
         isOpen = {isOpen}
         onRequestClose ={requestClose}
         style={{
+ 
            overlay: {
+        
             backgroundColor: 'rgba(0,0,0,0.5)',
             display:isOpen ? 'block' : 'none',
             alignItems: 'center',
             justifyContent: 'center',
             position: 'fixed',
+            overflow:'hidden',
             top: 0,
             left: 0,
             right:0,
@@ -108,15 +112,18 @@ const ListProperty =({isOpen,requestClose})=>{
             left: '50%',
             transform: 'translate(-50%,-50%)',
             height: '100%',
-            width: '40%',
+            width: '26%',
             backgroundColor: 'white',
+            overflow:'auto',
 
            }
         }}
+        contentLabel="Example Modal"
 >
-<div style={{width: '100%'}}>
+    <div id="modal-content">
 {getPageContent()}
 </div>
+
 </Modal>
 
     );
